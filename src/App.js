ad9d40ce5392b68e919dcecd1ed18e22fd8a24a0
Eduarda <eduardapinheiro1302@gmail.com>
2021-08-26 10:51:55 -0300

    feat: create first input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import Form from './components/Form'
 import Main from './components/Main'
+import Form from './components/Form'
+import Field from './components/Field'
 
 function App() {
   return (
     <Main>
-      <Form></Form>
+      <Form>
+        <Field.Text label="Email" name="email" type="email" />
+      </Form>
     </Main>
   )
 }
---
+++ src/components/Content/index.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+const Content = styled.span``
+
+export default Content
