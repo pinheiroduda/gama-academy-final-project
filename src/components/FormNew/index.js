b813e4a35b61a771d66c98f3dc0cb59ad42c0af3
Eduarda <eduardapinheiro1302@gmail.com>
2021-08-28 11:58:16 -0300

    feat: re-add input fields

--- src/components/FormNew/index.js
+++ src/components/FormNew/index.js
@@ -4,8 +4,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 
 import schema from './validation'
 import Field from '../Field/index'
-import FormStyle from '../Form/style'
-
+import FormStyle, { FormDocumentStyle, Input } from '../Form/style'
 const FormNew = () => {
   const {
     register,
@@ -20,13 +19,53 @@ const FormNew = () => {
   }
 
   return (
-    <FormStyle onSubmit={handleSubmit(newUser)}>
-      <Field.Text label="Nome" type="text" {...register('name')} />
-      {errors?.name?.message}
-      <Field.Text label="Email" type="email" {...register('email')} />
-      {errors?.email?.message}
-      <input type="submit" value="Enviar" />
-    </FormStyle>
+    <>
+      <FormStyle onSubmit={handleSubmit(newUser)}>
+        <Field.Text label="Nome" type="text" {...register('name')} />
+        {errors?.name?.message}
+        <Field.Text
+          label="Cargo pretendido"
+          type="text"
+          {...register('position')}
+        />
+        {errors?.position?.message}
+        <select {...register('gender')}>
+          <option value="female">Feminino</option>
+          <option value="male">Masculino</option>
+          <option value="other">Outro</option>
+        </select>
+        <select {...register('marital-status')}>
+          <option value="married">Casado(a)</option>
+          <option value="single">Solteiro(a)</option>
+          <option value="other">Outro</option>
+        </select>
+        <Field.Text label="Email" type="email" {...register('email')} />
+        {errors?.email?.message}
+        <Field.Text label="Celular" type="tel" {...register('tel')} />
+        {errors?.tel?.message}
+        <Field.Text
+          label="Data de nascimento"
+          type="date"
+          {...register('date')}
+        />
+        {errors?.date?.message}
+        <Field.Text label="Endereço" type="text" {...register('address')} />
+        {errors?.address?.message}
+        <Field.Text label="Bairro" type="text" {...register('district')} />
+        {errors?.district?.message}
+        <Field.Text label="Cidade" type="text" {...register('city')} />
+        {errors?.city?.message}
+      </FormStyle>
+      <FormDocumentStyle>
+        <h1> Documentos </h1>
+        <br></br>
+        <Field.Text label="Identidade" type="text" {...register('email')} />
+        {errors?.email?.message}
+        <Field.Text label="CPF" type="text" {...register('tel')} />
+        {errors?.tel?.message}
+      </FormDocumentStyle>
+      <Input type="submit" value="Enviar" />
+    </>
   )
 }
 
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,12 +1,9 @@
 import styled from 'styled-components'
 
-const Main = styled.div`  
-  background: linear-gradient(#093028, #237A57);
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify content: space-between;
-  padding: 10px;
+const Main = styled.div`
+  background: linear-gradient(#093028, #237a57);
+  width: 100vw;
+  height: 100vh;
 `
 
 export default Main
