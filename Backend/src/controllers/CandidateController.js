848977c00bc3f6ad924f7b6a5d5f7b4a924f1d32
Eduarda <eduardapinheiro1302@gmail.com>
2021-08-29 18:18:11 -0300

    feat: update database fields

--- Backend/src/controllers/CandidateController.js
+++ Backend/src/controllers/CandidateController.js
@@ -2,19 +2,40 @@ const Candidate = require('../models/Candidate')
 
 module.exports = {
   async register(req, res) {
-    const { email, gender, name, cep } = req.body
+    const {
+      name,
+      position,
+      date,
+      gender,
+      email,
+      phone,
+      cep,
+      logradouro,
+      district,
+      city,
+      id,
+      cpf
+    } = req.body
 
     const newCandidate = new Candidate()
 
-    newCandidate.email = email
     newCandidate.name = name
+    newCandidate.position = position
+    newCandidate.date = date
     newCandidate.gender = gender
+    newCandidate.email = email
+    newCandidate.phone = phone
     newCandidate.cep = cep
+    newCandidate.logradouro = logradouro
+    newCandidate.district = district
+    newCandidate.city = city
+    newCandidate.id = id
+    newCandidate.cpf = cpf
 
     newCandidate.save((err, savedCandidate) => {
       if (err) {
         console.log(err)
-        return res.status(500).send('iti malia deu probleminha!')
+        return res.status(500).send('erro!')
       }
 
       return res.status(200).send(savedCandidate)
--- Backend/src/models/Candidate.js
+++ Backend/src/models/Candidate.js
@@ -3,9 +3,17 @@ const mongoose = require('mongoose')
 const CandidateSchema = new mongoose.Schema(
   {
     name: { type: String, unique: false, required: true },
-    email: { type: String, unique: true, required: true },
+    position: { type: String, unique: false, required: true },
+    date: { type: Date, unique: false, required: true },
     gender: { type: String, unique: false, required: true },
-    cep: { type: String, unique: false, required: true }
+    email: { type: String, unique: true, required: true },
+    phone: { type: String, unique: true, required: true },
+    cep: { type: String, unique: false, required: true },
+    logradouro: { type: String, unique: false, required: true },
+    district: { type: String, unique: false, required: true },
+    city: { type: String, unique: false, required: true },
+    id: { type: String, unique: true, required: true },
+    cpf: { type: String, unique: true, required: true }
   },
   {
     timestamps: true
